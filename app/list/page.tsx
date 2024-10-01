8c4eac5e21cde88f677da87144b94d75c0b07fec
tuanna-kite <tuanna.kiteacademy@gmail.com>
2024-10-01 23:50:09 +0700

    buoi 1 - easy version

---
+++ app/list/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaskListPage = () => {
+  return (
+    <div>TaskListPage</div>
+  )
+}
+
+export default TaskListPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,147 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
 
-export default function Home() {
+/**
+ * CRUD
+ * 1. Tao task moi - Done
+ * 2. Xem danh sach task - Done
+ * 3. Xoa task - Done
+ * 4. Sua task - Done
+ */
+
+type Task = string;
+
+type TaskItemProps = {
+  task: Task;
+  index: number;
+  onDelete?: (index: number) => void;
+  onUpdate?: (index: number, newTask: Task) => void;
+};
+
+const TaskItem = ({ index, task, onDelete, onUpdate }: TaskItemProps) => {
+  const [isEdit, setIsEdit] = React.useState<boolean>(false);
+  const [text, setText] = React.useState<string>(task);
+
+  const toggleEdit = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const onEditClicked = () => {
+    if (!isEdit) {
+      toggleEdit();
+    } else {
+      if (text.trim() === '') {
+        alert('Task name is required');
+        return;
+      }
+      setText(text);
+      if (onUpdate) {
+        onUpdate(index, text);
+      }
+      toggleEdit();
+    }
+  };
+
+  const onDeleteClicked = () => {
+    if (onDelete) {
+      onDelete(index);
+    }
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <li className='w-80 flex p-2 border border-gray-800 rounded-lg'>
+      {isEdit ? (
+        <input
+          placeholder='Nhap task'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      ) : (
+        <span className='flex-1'>{task}</span>
+      )}
+      <div className='space-x-2'>
+        <button
+          onClick={onEditClicked}
+          className='py-1 px-3 bg-blue-700 text-white text-sm rounded-lg'
+        >
+          {isEdit ? 'Save' : 'Edit'}
+        </button>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={onDeleteClicked}
+          className='py-1 px-3 bg-red-700 text-white text-sm rounded-lg'
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          Del
+        </button>
+      </div>
+    </li>
+  );
+};
+
+const HomePage = () => {
+  const [tasks, setTasks] = React.useState<Task[]>([]);
+  const [text, setText] = React.useState<string>('');
+
+  const createTask = () => {
+    if (text.trim() === '') {
+      alert('Task name is required');
+      return;
+    }
+    setTasks([...tasks, text]);
+    setText('');
+  };
+
+  const deleteTask = (index: number) => {
+    tasks.splice(index, 1);
+    setTasks([...tasks]);
+  };
+
+  const updateTask = (index: number, newTask: Task) => {
+    tasks[index] = newTask;
+    setTasks([...tasks]);
+  };
+
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center space-y-8'>
+      <h1 className='text-3xl font-semibold'>Todo List</h1>
+      <div className='space-x-4 w-80 flex'>
+        <input
+          type='text'
+          placeholder='Enter task name'
+          className='border p-2 rounded-md flex-1'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button
+          onClick={createTask}
+          className='bg-black text-white py-2 px-3 rounded-md hover:bg-black/80'
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Add
+        </button>
+      </div>
+
+      <div className='w-80 space-y-4'>
+        <h2 className='text-center text-xl font-semibold text-blue-900'>
+          Danh sách
+        </h2>
+        {tasks.length === 0 ? (
+          <p className='text-center text-gray-600'>Không có task nào</p>
+        ) : (
+          <ul className='space-y-4'>
+            {tasks.map((task, idx) => (
+              <TaskItem
+                task={task}
+                key={task}
+                index={idx}
+                onUpdate={updateTask}
+                onDelete={deleteTask}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
-}
+};
+
+export default HomePage;
